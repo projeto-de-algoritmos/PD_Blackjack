650b832a184b115611d73629eb149ef57d685479
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-31 23:01:09 -0300

    chore: adding files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import HomePage from './Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './global/reset.css';
+import './global/global.css';
+
+function App() {
+  return (
+    <div className='main-container'>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/utils/images.js
@@ -0,0 +1,17 @@
+import blackJackTittle from '../images/blackjack-tittle.png';
+import paus from '../images/paus.png';
+import copas from '../images/copas.png';
+import espadas from '../images/espadas.png';
+import ouro from '../images/ouro.png';
+import dica from '../images/dica.png';
+
+const images = {
+    backJackTittle: blackJackTittle,
+    paus: paus,
+    copas: copas,
+    espadas: espadas,
+    ouro: ouro,
+    dica: dica
+}
+
+export default images;
