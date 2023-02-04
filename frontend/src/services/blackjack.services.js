0d5dc4157c98e25e6439fcd5221e28d5e7e58279
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-02-04 19:50:18 -0300

    feat: adding new services

--- frontend/src/services/blackjack.services.js
+++ frontend/src/services/blackjack.services.js
@@ -4,10 +4,15 @@ export async function startGame() {
     return api.get(`/start`);
 }
 
-export async function getCard(sum_cards) {
+export async function getUserCard(sum_cards) {
     return api.get(`/get_card/${sum_cards}`);
+}
+
+export async function getDealerCard(sum_cards) {
+    return api.get(`/get_dealercard/${sum_cards}`);
 }
 
 export async function getHint(sum_cards) {
     return api.get(`/get_hint/${sum_cards}`);
 }
+
