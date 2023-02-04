48b27b711a4c468635ce95e7fb64fce0b3500551
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-02-04 19:48:30 -0300

    styles: update styles

--- frontend/src/Card/Card.jsx
+++ frontend/src/Card/Card.jsx
@@ -5,24 +5,51 @@ import "./index.css";
 const Card = ({ numero, naipe }) => {
   return (
     <div className="card-container">
-      <div className="card-header">
-        <p>{numero}</p>
-      </div>
-      <div className="card-main">
-        <img
-          src={
-            naipe === 0
-              ? images.paus
-              : naipe === 1
-              ? images.copas
-              : naipe === 2
-              ? images.espadas
-              : images.ouro
-          }
-          alt="Simbolo Black Jack"
-          width={50}
-        />
-      </div>
+      {numero > 1 && numero <= 10 && (
+        <>
+          <div className="card-header">
+            <p>{numero}</p>
+          </div>
+          <div className="card-main">
+            <img
+              src={
+                naipe === 0
+                  ? images.paus
+                  : naipe === 1
+                  ? images.copas
+                  : naipe === 2
+                  ? images.espadas
+                  : images.ouro
+              }
+              alt="Simbolo Black Jack"
+              width={50}
+            />
+          </div>
+        </>
+      )}
+      {console.log("NUMEROOOO", numero)}
+      {(numero == 1 || numero > 10) && (
+        <>
+          <div className="card-header">
+            <p>{numero === 11 ? "K" : numero === 12 ? "Q" : "J"}</p>
+          </div>
+          <div className="card-main">
+            <img
+              src={
+                numero === 1
+                  ? images.espadas
+                  : numero === 11
+                  ? images.rei
+                  : numero === 12
+                  ? images.rainha
+                  : images.valete
+              }
+              alt="Simbolo Black Jack"
+              width={50}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
