e3e89f53936051ce8b58ea8059ba465c54aa1f43
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-02-04 19:50:52 -0300

    chore: adding files

--- frontend/src/utils/images.js
+++ frontend/src/utils/images.js
@@ -4,6 +4,10 @@ import copas from '../images/copas.png';
 import espadas from '../images/espadas.png';
 import ouro from '../images/ouro.png';
 import dica from '../images/dica.png';
+import rei from '../images/rei.png';
+import rainha from '../images/rainha.png';
+import valete from '../images/valete.png';
+
 
 const images = {
     backJackTittle: blackJackTittle,
@@ -11,7 +15,10 @@ const images = {
     copas: copas,
     espadas: espadas,
     ouro: ouro,
-    dica: dica
+    dica: dica,
+    rei: rei,
+    rainha: rainha,
+    valete: valete
 }
 
 export default images;
